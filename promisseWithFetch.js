c6ddcf252ce7f0e1602aeb5399caabd477eb3ade
lucas <lucasgominhaa448@gmail.com>
2022-12-02 00:05:27 -0300

    added new fetch

--- promisseWithFetch.js
+++ promisseWithFetch.js
@@ -1,3 +1,14 @@
-fetch("https://jsonplaceholder.typicode.com/todos").then(response => {
-  response.json().then(data => console.log(data));
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(response => {
+  response.json()
+  .then(data => console.log(data));
 });
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+  response.json()
+  .then(data => {
+    console.log(data);
+    
+  })
+})
